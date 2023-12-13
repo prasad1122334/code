38f6cb1c232d1ae18d29468f9079edd8cfbddd91
prasad1122334 <153738462+prasad1122334@users.noreply.github.com>
2023-12-13 20:59:59 +0530

    Update Studentdata.cpp

--- Studentdata.cpp
+++ Studentdata.cpp
@@ -1,131 +1,131 @@
+/*
+Develop a program in C++ to create a database of student’s information system containing the
+following information: Name, Roll number, Class, Division, Date of Birth, Blood group, Contact
+address, Telephone number, Driving license no. and other. Construct the database with
+suitable member functions. Make use of constructor, default constructor, copy constructor,
+destructor, static member functions, friend class, this pointer, inline code and dynamic
+memory allocation operators-new and delete as well as exception handling.
+*/
+
 #include<iostream>
+#include<string.h>
+#include<typeinfo>
+#include<stdexcept>
+
 using namespace std;
 
 class Information
 {
+char license[10] , address[20];
+long int contact ;
 
-	public:
-		long phoneno;
-		char* address;
-		char blood[3];
-		int height;
+    public:
 
-		Information()
-		{
-			phoneno = 0;
-			height = 0.0;
-			blood = "X++";
-			address = new char[50];	
-		}
- 
-		friend class Student;
-
-		~Information()
-		{
-			delete address;
-		}
-
-};
-class Student //public Information
+Information() //Default constructor;
 {
+strcpy(address,"ABC");
+strcpy(license,"XY-0000000000");
+contact=0000000000;
+}
 
-	char* name;
-	int rollno ;
-	char* dob ;
-	char div;
-	char year[2];
-	//Information* ptr;
- 
-	
-	public:
-		Student()
-		{
+~Information() //Destructor;
+{
+cout<<"I am in Destructor";
+}
 
-			name = new char[50];
-			rollno = 0;
-			dob = new char[10];
-			div = 'X';
-			year = "";
-			ptr = new Information;
-		}
-
-		Student(Student& s)
-		{
-
-		}
-
-		static void setStudent()
-		{
-			cout<<"Enter the name of the Student : ";
-			cin >> (*name);
-
-			cout<<"Enter Date of Birth of Student : ";
-			cin>>(*dob);
-
-			cout<<"Enter Year of Student : ";
-			cin>> year;
-
-			cout<<"Enter Division of Student : ";
-			cin>> div;
-
-			cout<<"Enter Height of Student : ";
-			cin>> ptr->height;
-
-			cout<<"Enter Blood Group of Student : ";
-			cin>> ptr->blood;
-
-			cout<<"Enter Phone No. of Student : ";
-			cin>> ptr->phoneno;
-
-			cout<<"Enter Year of Student : ";
-			cin>> year;
-
-			cout<<"Enter Address of Student : ";
-			cin>> ptr ->address;
-
-		}
-
-		inline void showStudent()
-		{
-
-			cout<<"\n\n\t\tStudent Details are : ";
-			cout<<"\tName : " << *name <<endl;
-			cout<<"\tDate of Birth : " << *dob <<endl;
-			cout<<"\tYear : " << year <<endl;
-			cout<<"\tDivision : " << div <<endl;
-			cout<<"\tRoll No. : " << rollno <<endl;
-			cout<<"\tBlood: " << ptr->blood<<endl;
-			cout<<"\tPhone No. : " << ptr->phoneno<<endl;
-			//Exception handling for height;
-			try
-			{
-				if (ptr->height < 3)
-					throw ptr->height;
-				else
-					cout<<"\tHeight : " << ptr->height<<endl;
-			}
-			catch(int &h)
-			{
-				h = 4.0;
-
-				cout<<"\n\n\tERROR : Invalid height.\n\n\tHeight is set to default value 4.00 .\n\n";
-			}
-
-			cout<<"\tAddress : " << ptr->address <<endl;
-		}
-
-		~Student()
-		{
-			delete name;
-			delete dob;
-		}
+friend class Person;
 
 };
-static int count = 0;
+
+class Person
+{
+char blood[5] , name[20] , dob[10] , division , year[3] ;
+static int rollno ;
+float ht , wt ;
+Information* ptr;
+    public :
+   
+Person() //Default constructor
+{
+strcpy(name,"XYZ");
+strcpy(dob,"dd/mm/yy");
+strcpy(blood,"A +");
+division = 'X';
+char* year;
+year = new char[5];
+strcpy(year , "FE");
+ht=0.0;
+wt=0.0;
+ptr = new Information;
+}
+
+void getdata()
+{
+cout <<"Enter name of the student : ";
+cin >> name;
+
+cout <<"Enter Date of Birth : ";
+cin >> dob;
+
+cout <<"Enter BloodGroup of the student : ";
+cin >> blood;
+
+cout <<"Enter height: ";
+cin >> ht;
+
+try
+{
+if (ht<4.0)
+{
+   ht = 0.0;
+throw(ht);
+}
+}
+catch(float ht)
+{
+//throw runtime_error("\n height should greater than 4 ft");
+cout<<"Invalid height : Height is set back to 0.0\n";
+ht = 0.0;
+}
+
+cout <<"Enter weight: ";
+cin >> wt;
+
+cout <<"Enter Year: ";
+cin >> year;
+
+cout <<"Enter Division: ";
+cin >> division;
+
+cout<< "Enter Licence no. : ";
+cin >> ptr ->license;
+
+cout<< "Enter Contact no. : ";
+cin >> ptr ->contact;
+
+}
+
+void showdata();
+
+};
+void Person :: showdata()
+{
+cout << " Name : " << name <<endl;
+cout << " DOB : "<< dob<<endl;
+cout << " Year : "<< year<<endl;
+cout << " Division : "<< division<<endl;
+cout << " Height : "<< ht<<endl;
+cout << " Weight : "<< wt <<endl;
+cout << " Licence no. : "<< ptr ->license <<endl;
+cout << " Contact no. : "<< ptr ->contact <<endl;
+
+}
+
 int main()
 {
-	Student S1;
-	S1.setStudent();
-	S1.showStudent();
-	return 0;
+Person P1;
+cout<< "Enter Data for a person\n\n";
+P1.getdata();
+P1.showdata();
+
 }
